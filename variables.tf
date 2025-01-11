a14847a68a7499aec7f5da82dbb31d0202280a01
betrand <tekeh.betrand@gmail.com>
2025-01-11 01:12:40 -0500

    New commit

--- variables.tf
+++ variables.tf
@@ -20,7 +20,6 @@ variable "AMIS" {
         us-east-1 = "ami-0f40c8f97004632f9"
         us-east-2 = "ami-05692172625678b4e"
         us-west-2 = "ami-0352d5a37fb4f603f"
-        us-west-1 = "ami-0f40c8f97004632f9"
     }
 }
 
